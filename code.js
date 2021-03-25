aef6a29e3a147a9fcb2c0454429ed0673cd7c419
bro abbas <75854041+abbasalkabbi@users.noreply.github.com>
2021-03-25 21:29:31 +0300

    Add files via upload

---
+++ code.js
@@ -0,0 +1,11 @@
+var menu=document.getElementById('nav');
+
+function play(){
+    if(menu.style.display==="block"){
+        menu.style.display="none";
+    }else{
+        menu.style.display="block";  
+    }
+    
+}
+
